evil0sheep/motorcar

commit d13ce44e2c7366800b9fdcc29b82e972d934fba2

Remove double to float casts within source.
Changed all float declerations so they contain an 'f' in their numeric rhv.  This avoids a double to float conversion that would have to take place.

src/examples/compositors/rift-hydra-compositor/main.cpp
@@ -68,10 +68,10 @@ int main(int argc, char *argv[])
         compositor->setDisplay(hmd);
     }else{
         std::cout << "Using Default Display" << std::endl;
-        float camToDisplayDistance = 0.1;
-        motorcar::Display *display = new motorcar::Display(context, glm::vec2(0.325, 0.1), scene, glm::translate(glm::mat4(1), glm::vec3(0, 0.8, 1.25))
-                                                                                                 * glm::rotate(glm::mat4(1), -25.f, glm::vec3(1, 0, 0)));
-        display->addViewpoint(new motorcar::ViewPoint( .01, 100, display, display, glm::translate(glm::mat4(1), glm::vec3(0, 0.0, camToDisplayDistance))));
+        float camToDisplayDistance = 0.1f;
+        motorcar::Display *display = new motorcar::Display(context, glm::vec2(0.325f, 0.1f), scene, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.8f, 1.25f))
+                                                                                                 * glm::rotate(glm::mat4(1.0f), -25.0f, glm::vec3(1.0f, 0.0f, 0.0f)));
+        display->addViewpoint(new motorcar::ViewPoint( .01.0f, 100.0f, display, display, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, camToDisplayDistance))));
         compositor->setDisplay(display);
     }
 
@@ -94,11 +94,11 @@ int main(int argc, char *argv[])
         //ds325->setParentNode(handController);
         //ds325->setParentNode(compositor->display());
 
-        baseNode->setTransform(glm::translate(glm::mat4(1), glm::vec3(0.5,0.25,.25)));
+        baseNode->setTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.5f,0.25f,.25f)));
 
         handController->setPointingDevice(new motorcar::SixDOFPointingDevice(compositor->defaultSeat(),handController));
 
-        headController->setBoneTracker(new motorcar::SingleBoneTracker(skeleton->headBone(), glm::translate(glm::mat4(), glm::vec3(0, .073, .184)),
+        headController->setBoneTracker(new motorcar::SingleBoneTracker(skeleton->headBone(), glm::translate(glm::mat4(), glm::vec3(0.0f, .073f, .184f)),
                                                                        skeleton, baseNode));
 
         std::cout << "parenting display to controller "<<std::endl;
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
 
 
 
-          glm::vec3 displayPosition = glm::vec3(0, .127, -.165);
+          glm::vec3 displayPosition = glm::vec3(0.0f, .127f, -.165f);
 
           glm::mat4 displayTransform = glm::translate(glm::mat4(), displayPosition);
 

src/examples/compositors/simple-compositor/main.cpp
@@ -57,9 +57,9 @@ int main(int argc, char *argv[])
 
    
     std::cout << "Using Default Display" << std::endl;
-    float camToDisplayDistance = 0.1;
-    motorcar::Display *display = new motorcar::Display(context, glm::vec2(0.325, 0.1), scene, glm::translate(glm::mat4(1), glm::vec3(0, 0.8, 1.25))                                                                                                 * glm::rotate(glm::mat4(1), -25.f, glm::vec3(1, 0, 0)));
-    display->addViewpoint(new motorcar::ViewPoint( .01, 100, display, display, glm::translate(glm::mat4(1), glm::vec3(0, 0.0, camToDisplayDistance))));
+    float camToDisplayDistance = 0.1f;
+    motorcar::Display *display = new motorcar::Display(context, glm::vec2(0.325f, 0.1f), scene, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.8f, 1.25f))                                                                                                 * glm::rotate(glm::mat4(1.0f), -25.0f, glm::vec3(1.0f, 0.0f, 0.0f)));
+    display->addViewpoint(new motorcar::ViewPoint( .01f, 100.0f, display, display, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, camToDisplayDistance))));
     compositor->setDisplay(display);
     scene->addDisplay(compositor->display());
 

src/compositor/geometry.cpp
@@ -48,7 +48,7 @@ Geometry::Ray::Ray(glm::vec3 p, glm::vec3 d)
 
 Geometry::Ray Geometry::Ray::transform(glm::mat4 t) const
 {
-    return Ray(glm::vec3(t * glm::vec4(p, 1)), glm::vec3(t * glm::vec4(d, 0)));
+    return Ray(glm::vec3(t * glm::vec4(p, 1.0f)), glm::vec3(t * glm::vec4(d, 0)));
 }
 
 glm::vec3 Geometry::Ray::solve(float t) const
@@ -65,13 +65,13 @@ void Geometry::Ray::draw(SceneGraphNode *parent, glm::vec3 color, glm::mat4 tran
 {
     glm::mat4 translation = glm::translate(glm::mat4(), p);
     float vertices[]= {
-        -0.05, 0, 0,
-        0.05, 0, 0,
-        0, -0.05, 0,
-        0, 0.05, 0,
-        0, 0, -0.05,
-        0, 0, 0.05,
-        0, 0, 0,
+        -0.05f, 0.0f, 0.0f,
+        0.05f, 0.0f, 0.0f,
+        0.0f, -0.05f, 0.0f,
+        0.0f, 0.05f, 0.0f,
+        0.0f, 0.0f, -0.05f,
+        0.0f, 0.0f, 0.05f,
+        0.0f, 0.0f, 0.0f,
         d.x, d.y, d.z
 
     };

src/compositor/qt/qtwaylandmotorcarcompositor.cpp
@@ -278,7 +278,7 @@ void QtWaylandMotorcarCompositor::surfaceMapped()
             motorcar::WaylandSurface::SurfaceType surfaceType;
 
             int type = static_cast<int>(surface->windowType());
-            float popupZOffset = 0.05;
+            float popupZOffset = 0.05f;
 
 
             if(type == QWaylandSurface::WindowType::Toplevel){

src/compositor/qt/qtwaylandsurfacenode.cpp
@@ -115,11 +115,11 @@ void QtwaylandSurfaceNode::paintChildren(QWaylandSurface *surface, QWaylandSurfa
 
 void QtwaylandSurfaceNode::computeSurfaceTransform(float ppcm)
 {
-    if(ppcm > 0){
+    if(ppcm > 0.0f){
         float ppm = ppcm * 100.f;
-        glm::mat4 surfaceRotation = glm::rotate(glm::mat4(1), 180.f ,glm::vec3(0, 0, 1));
-        glm::mat4 surfaceScale = glm::scale(glm::mat4(1), glm::vec3( m_surface->size().width() / ppm,  m_surface->size().height() / ppm, 1));
-        glm::mat4 surfaceOffset = glm::translate(glm::mat4(1), glm::vec3(-0.5f, -0.5f, 0));
+        glm::mat4 surfaceRotation = glm::rotate(glm::mat4(1), 180.0f ,glm::vec3(0.0f, 0.0f, 1.0f));
+        glm::mat4 surfaceScale = glm::scale(glm::mat4(1), glm::vec3( m_surface->size().width() / ppm,  m_surface->size().height() / ppm, 1.0f));
+        glm::mat4 surfaceOffset = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, -0.5f, 0.0f));
         m_surfaceTransform = surfaceRotation * surfaceScale * surfaceOffset  ;
 
     }
@@ -137,7 +137,7 @@ QtwaylandSurfaceNode *QtwaylandSurfaceNode::getSurfaceNode(const QWaylandSurface
 
 bool QtwaylandSurfaceNode::computeLocalSurfaceIntersection(const Geometry::Ray &localRay, QPointF &localIntersection, float &t) const
 {
-    Geometry::Plane surfacePlane = Geometry::Plane(glm::vec3(0), glm::vec3(0,0,1));
+    Geometry::Plane surfacePlane = Geometry::Plane(glm::vec3(0.0f), glm::vec3(0.0f,0.0f,1.0f));
     if(glm::dot(localRay.d, surfacePlane.n) == 0) return false;
 
     Geometry::Ray transformedRay = localRay.transform(glm::inverse(surfaceTransform()));

src/compositor/qt/textureblitter.cpp
@@ -101,10 +101,10 @@ void TextureBlitter::drawTexture(int textureId, const QRectF &targetRect, const 
     GLfloat zValue = depth / 1000.0f;
     //Set Texture and Vertex coordinates
     const GLfloat textureCoordinates[] = {
-        0, 0,
-        1, 0,
-        1, 1,
-        0, 1
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f
     };
 
     GLfloat x1 = targetRect.left();

src/compositor/scenegraph/input/sixdofpointingdevice.cpp
@@ -51,12 +51,12 @@ SixDOFPointingDevice::SixDOFPointingDevice(Seat *seat, PhysicalNode *parent, con
     ,m_sixDofFocus(NULL)
 { 
     float vertices[]= {
-        -0.05, 0, 0,
-        0.05, 0, 0,
-        0, -0.05, 0,
-        0, 0.05, 0,
-        0, 0, -1,
-        0, 0, 0.05,
+        -0.05f, 0.0f, 0.0f,
+        0.05f, 0.0f, 0.0f,
+        0.0f, -0.05f, 0.0f,
+        0.0f, 0.05f, 0.0f,
+        0.0f, 0.0f, -1.0f,
+        0.0f, 0.0f, 0.05f,
 
 
     };
@@ -83,7 +83,7 @@ void SixDOFPointingDevice::handleFrameBegin(Scene *scene)
 {std::map<WaylandSurface *, WaylandSurfaceNode *> m_surfaceMap;
     PhysicalNode::handleFrameBegin(scene);
 
-    Geometry::Ray ray = Geometry::Ray(glm::vec3(0,0,0), glm::vec3(0,0,-1)).transform(worldTransform());
+    Geometry::Ray ray = Geometry::Ray(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,0.0f,-1.0f)).transform(worldTransform());
 
     Geometry::RaySurfaceIntersection *intersection = scene->intersectWithSurfaces(ray);
 
@@ -100,7 +100,7 @@ void SixDOFPointingDevice::handleFrameBegin(Scene *scene)
            glm::vec3 position = glm::vec3(surfaceNode->surfaceTransform() *
                                           glm::vec4((intersection->surfaceLocalCoordinates +
                                                      (glm::vec2(cursor->surface()->size())/2.0f) - glm::vec2(m_seat->pointer()->cursorHotspot())) /
-                                                    glm::vec2(surfaceNode->surface()->size()),0.01,1));
+                                                    glm::vec2(surfaceNode->surface()->size()),0.01f,1.0f));
 
            cursor->setParentNode(surfaceNode);
            cursor->setTransform(glm::translate(glm::mat4(), position));

src/compositor/scenegraph/output/display/rendertotexturedisplay.cpp
@@ -200,7 +200,7 @@ RenderToTextureDisplay::~RenderToTextureDisplay()
 void RenderToTextureDisplay::prepareForDraw()
 {
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-    glClearColor(0.f, 0.f, 0.f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 

commit d15df5811fe7b47c60c9ca09b3616a0eddb90b4d

fixed typo bug introduced in d13ce44e2c7366800b9fdcc29b82e972d934fba2

src/examples/compositors/rift-hydra-compositor/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
         float camToDisplayDistance = 0.1f;
         motorcar::Display *display = new motorcar::Display(context, glm::vec2(0.325f, 0.1f), scene, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.8f, 1.25f))
                                                                                                  * glm::rotate(glm::mat4(1.0f), -25.0f, glm::vec3(1.0f, 0.0f, 0.0f)));
-        display->addViewpoint(new motorcar::ViewPoint( .01.0f, 100.0f, display, display, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, camToDisplayDistance))));
+        display->addViewpoint(new motorcar::ViewPoint( 0.01f, 100.0f, display, display, glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, camToDisplayDistance))));
         compositor->setDisplay(display);
     }
 